eb28c8356ff165c8cb3248cfad4675d970825f7a
Hwiyun <Hwiyun>
2023-01-09 14:06:13 +0900

    select #2

--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -22,13 +22,79 @@ desc pet;
 describe pet;
 
 -- table 삭제
-drop table pet;
+-- drop table pet;
 show tables;
 
 -- insert: DML(C)
 insert 
 into pet 
-values('엥','응','옹','읭', '11','2023-01-06', null);
+values('휘윤','나','사람','1','1997-11-26',NULL);
 
+-- select: DML(R)
 select * from pet;
+
+-- update: DML(U)
+update pet
+	set name = '휘윤'
+	where name = '나';
+
+update pet
+		set death = null
+where name = '윤';
+
+-- delete: DML(D)
+delete from pet where owner = '나';
+	
+-- loaddata
+load data local infile 'd:\pet.txt' into table pet;
+
+-- select 연습
+select name, species
+	from pet
+where name = '윤';
+
+select name, species
+	from pet
+where birth >= '1997-01-01';
+
+select name, species, gender
+	from pet
+where birth = 'cat'
+	and gender = 'f';
+    
+select name, species
+	from pet
+where species = 'snake'
+	or species = 'bird';
+    
+  select name, birth
+	from pet
+order by birth desc;
+
+select name, birth, death
+  from pet
+ where death is not null;
+ 
+select name
+  from pet
+ where name like 'b%';
+
+select name
+  from pet
+ where name like '%fy';
+
+select name
+  from pet
+ where name like '%w%';
+
+select name
+  from pet
+ where name like '_____';
+ 
+select name
+  from pet
+ where name like 'b____';
+
+select count(*)
+  from pet
 
